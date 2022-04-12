238913697bb7e7022b7a2f41cfce27ef6b979801
CodeServer <CodeServer@jlu42.com>
2022-04-12 10:18:56 +0100

    added tags detail

--- src/components/repositoryInfo/RepositoryInfo.tsx
+++ src/components/repositoryInfo/RepositoryInfo.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { useLocation, Link } from "react-router-dom";
-import { ListItemButton, Button, Box, Card, CardContent, Typography, CardActions, Grid, List, ListItemText, Snackbar, Alert, Tooltip } from "@mui/material";
+import { ListItemButton, Button, Box, Card, CardContent, Typography, CardActions, Grid, List, ListItemText, Snackbar, Alert, Tooltip, Collapse } from "@mui/material";
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import useRepositories from "../../hooks/useRepositories";
 import Tag from "../../interfaces/Tag";
 import Repository from "../../interfaces/Repositoriy";
 import { getHostNameFromURL, printSize, printTimePassed, PrintOSIcon, toUpperFirst, printMonth } from "../../utils";
 import RepoNotFound from "./RepoNotFound"
 import Loading from "../Loading";
+import { digestDisplay } from "../../interfaces/Utils";
 
 const RepositoryInfo: React.FC = () => {
     const location = useLocation();
@@ -15,15 +18,26 @@ const RepositoryInfo: React.FC = () => {
     const  { isFetched, repositories } = useRepositories();
     const [hideTags, setHideTags] = React.useState(true);
     const [snackbarOpen, setSnackbarOpen] = React.useState(false);
+    const [showDetails, setShowDetails] = React.useState(false);
     const repository = repositories.find((element) => (element.name === path.slice(12,path.length)));
 
-    const generatePushCommand = (hostName: string) => {
-        return "docker push " + hostName + "/" + (repository?.name ? repository?.name : "") + ":tagname";
+    const generatePushCommand = (hostName: string, tagName: string) => {
+        return "docker push " + hostName + "/" + (repository?.name ? repository?.name : "") + ":" + tagName;
     }
 
-    const handleClick = () => {
+    const generatePullCommand = (hostName: string, tagName: string) => {
+        return "docker pull " + hostName + "/" + (repository?.name ? repository?.name : "") + ":" + tagName;
+    }
+
+    const handlePushClick = () => {
         const hostName = getHostNameFromURL(process.env.REGISTRY_URL ? process.env.REGISTRY_URL : "");
-        navigator.clipboard.writeText(generatePushCommand(hostName));
+        navigator.clipboard.writeText(generatePushCommand(hostName,"tagname"));
+        setSnackbarOpen(true);
+    };
+
+    const handlePullClick = (tagName: string) => {
+        const hostName = getHostNameFromURL(process.env.REGISTRY_URL ? process.env.REGISTRY_URL : "");
+        navigator.clipboard.writeText(generatePullCommand(hostName,tagName));
         setSnackbarOpen(true);
     };
 
@@ -34,6 +48,10 @@ const RepositoryInfo: React.FC = () => {
     const toggleHide = () => {
         setHideTags(!hideTags);
     }
+
+    const toggleCollapse = () => {
+        setShowDetails((prev) => !prev);
+    };
     
     return (isFetched ? (
         repository ? (
@@ -61,7 +79,7 @@ const RepositoryInfo: React.FC = () => {
                                     <Typography className="lines" variant="body2">
                                         To push a new tag to this repository,
                                     </Typography>
-                                    <Box className="commandDisplay" onClick={handleClick} component="span" sx={{ display: 'block', bgcolor: '#445d6e', color: 'white', borderColor: '#445d6e' }}>{generatePushCommand(getHostNameFromURL(process.env.REGISTRY_URL ? process.env.REGISTRY_URL : ""))}</Box>
+                                    <Box className="commandDisplay" onClick={handlePushClick} component="span" sx={{ display: 'block', bgcolor: '#445d6e', color: 'white', borderColor: '#445d6e' }}>{generatePushCommand(getHostNameFromURL(process.env.REGISTRY_URL ? process.env.REGISTRY_URL : ""),"tagname")}</Box>
                                 </Box>
                             </Grid>
                         </Grid>
@@ -70,7 +88,7 @@ const RepositoryInfo: React.FC = () => {
                         <Button size="small">Learn More</Button>
                     </CardActions> */}
                 </Card>
-                <Card className="tagsDetail" variant="outlined">
+                <Card className="tagsOverview" variant="outlined">
                     <CardContent>
                         <Typography className="lines" variant="h6">
                             Tags
@@ -107,7 +125,7 @@ const RepositoryInfo: React.FC = () => {
                                     return result;
                                 }
                             }).slice(0,((hideTags && repository.tags.length > 4) ? 3 : repository.tags.length)).map((tag) => (
-                                <Grid container>
+                                <Grid className="lines" container>
                                     <Grid item sm={5}>
                                         <ListItemText primary={tag.label} />
                                     </Grid>
@@ -159,11 +177,75 @@ const RepositoryInfo: React.FC = () => {
                             )}
                         </List>
                     </CardContent>
+                </Card>
+                <Card className="detailsCard" variant="outlined">
                     <CardActions>
-                        <Button size="small">Details</Button>
+                        <Button startIcon={showDetails ? <ExpandLessIcon /> : <ExpandMoreIcon />} onClick={toggleCollapse} size="small">Tag Details</Button>
                     </CardActions>
                 </Card>
-                <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={handleClose}>
+                <Collapse className="tagsDetail" in={showDetails}>
+                    {repository.tags.map((tag) => (
+                        <Card className="tagCard" variant="outlined">
+                            <CardContent>
+                                <Grid container>
+                                    <Grid item lg={6} md={12}>
+                                        <Typography className="lines" variant="h5" component="div" color="#94a1aa">
+                                            TAG
+                                        </Typography>
+                                        <Typography className="lines" variant="h5" component="div" color="#007bff">
+                                            {tag.label}
+                                        </Typography>
+                                        <Typography className="lines" sx={{ mb: 1.5 }}>
+                                            Last updated: <b>{(tag.created ? printTimePassed(tag.created) : "")}</b>
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item lg={6} md={12}>
+                                        <Box sx={{ display: "flex", flexDirection: "column" }}>
+                                            <Typography className="lines" variant="h6">
+                                            To pull this image,
+                                            </Typography>
+                                            <Box className="commandDisplay" onClick={() => (handlePullClick(tag.label))} component="span" sx={{ display: 'block', bgcolor: '#445d6e', color: 'white', borderColor: '#445d6e' }}>{generatePullCommand(getHostNameFromURL(process.env.REGISTRY_URL ? process.env.REGISTRY_URL : ""),tag.label)}</Box>
+                                        </Box>
+                                    </Grid>
+                                    <Grid item lg={12} md={12}>
+                                        <Grid className="lines" container>
+                                            <Grid item sm={5}>
+                                                <ListItemText primary="DIGEST" />
+                                            </Grid>
+                                            <Grid item sm={4}>
+                                                <ListItemText primary="OS/ARCH" />
+                                            </Grid>
+                                            <Grid item sm={3}>
+                                                <ListItemText style={{display:'flex', justifyContent:'flex-end'}} primary="SIZE" />
+                                            </Grid>
+                                        </Grid>
+                                        
+                                        <List>
+                                            <ListItemButton 
+                                                className="lines"
+                                                component={Link}
+                                                to={"/repository/" + repository.name + "/" + tag.label}
+                                            >
+                                                <Grid container>
+                                                    <Grid item sm={5}>
+                                                        <ListItemText style={{color: '#007bff'}} primary={digestDisplay(tag.digest)} />
+                                                    </Grid>
+                                                    <Grid item sm={4}>
+                                                        <ListItemText primary={tag.os + "/" +tag.architecture} />
+                                                    </Grid>
+                                                    <Grid item sm={3}>
+                                                        <ListItemText style={{display:'flex', justifyContent:'flex-end'}} primary={tag.size ? printSize(tag.size) : "---"} />
+                                                    </Grid>
+                                                </Grid>
+                                            </ListItemButton>
+                                        </List>
+                                    </Grid>
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    ))}
+                </Collapse>
+                <Snackbar open={snackbarOpen} autoHideDuration={1500} onClose={handleClose}>
                     <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
                         Copied!
                     </Alert>
